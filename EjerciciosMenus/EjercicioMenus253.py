6f974b7216068a00641352a2cae27204ae154c9e
Hyrcanius <vicent.perezv@duocuc.cl>
2026-05-11 18:25:35 -0400

    opcion uno

--- EjerciciosMenus/EjercicioMenus253.py
+++ EjerciciosMenus/EjercicioMenus253.py
@@ -1,4 +1,5 @@
 menu = True
+deuda = 100000
 
 while menu:
     print("--MENU--")
@@ -9,9 +10,16 @@ while menu:
 
     if op == 1:
         print("Pago tarjeta de crédito.")
+        pago = int(input("Ingrese monto de pago: "))
+        if pago >= 0:
+            if pago <= deuda:
+                deuda = deuda - pago
+                print(f"Pago exitoso, su deuda es de: ${deuda}")
+            else:
+                print("Pago excede la deuda.")
+
     elif op == 2:
         print("Simulador de compras")
     elif op == 3:
         menu = False
-    
     
